37d9c5263385b87c89a85feef72be962bbc156b5
Щеврук <2chekomudyan24@gmail.com>
2026-04-09 12:07:59 +0300

    Добавлен файл JobListing.cs

---
+++ JobListing.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JobSearchApp
+{
+    public class JobListing
+    {
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; }
+
+        public JobListing(string jobTitle, string company, string description, string requirements)
+        {
+            JobTitle = jobTitle;
+            Company = company;
+            Description = description;
+            Requirements = requirements;
+        }
+
+        public string GetFullInfo()
+        {
+            return $"Вакансия: {JobTitle}\nКомпания: {Company}\nОписание: {Description}\nТребования: {Requirements}";
+        }
+
+        public override string ToString() => $"{JobTitle} в {Company}";
+    }
+}
